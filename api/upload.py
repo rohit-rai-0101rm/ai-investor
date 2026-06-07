c7245f7b4df60eea89400b863732d81937c2d038
Sandesh-hase <you@example.comsandeshhase15@gmail.com>
2026-06-07 17:12:03 +0530

    removed unused code

--- api/upload.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, UploadFile, File
-
-router = APIRouter()
-
-@router.post('/upload')
-async def upload_file(file: UploadFile = File(...)):
-    content = await file.read()
-    # TODO: process uploaded file content
-    return {'filename': file.filename, 'status': 'received'}
